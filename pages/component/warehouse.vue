beec2b836409aee62031ad919d1f971097da4411
1981697881 <1981697881@qq.com>
2020-08-26 20:20:12 +0800

    0.20

--- pages/component/warehouse.vue
+++ pages/component/warehouse.vue
@@ -1,26 +1,41 @@
 <template>
 	<view>
-	<cu-custom bgColor="bg-gradual-blue" :isBack="true"><block slot="backText">返回</block><block slot="content">仓库管理</block></cu-custom>
-	<scroll-view scroll-y class="page">
-		<view class="nav-list">
-			<navigator hover-class='none' :url="'/pages/component/warehouse/' + item.path" class="nav-li" navigateTo :class="'bg-'+item.color"
-			  v-for="(item,index) in elements" :key="index">
-				<view class="nav-title text-center">{{item.name}}</view>
-				<text :class="'cuIcon-' + item.cuIcon"></text>
-			</navigator>
-		</view>
-		<view class="cu-tabbar-height"></view>
-	</scroll-view>
+		<cu-custom bgColor="bg-gradual-blue" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">仓库管理</block>
+		</cu-custom>
+		<scroll-view scroll-y class="page">
+			<view class="nav-list">
+				<navigator
+					v-if="item.color == 'olive' ? false : true"
+					hover-class="none"
+					:url="'/pages/component/warehouse/' + item.path"
+					class="nav-li"
+					navigateTo
+					:class="'bg-' + item.color"
+					v-for="(item, index) in elements"
+					:key="index"
+				>
+					<view class="nav-title text-center">{{ item.name }}</view>
+					<text :class="'cuIcon-' + item.cuIcon"></text>
+				</navigator>
+				<view v-if="item.color == 'olive'" @tap="$manyCk(fabClick)" class="nav-li" :class="'bg-' + item.color" v-for="(item, index) in elements" :key="index">
+					<view class="nav-title text-center">{{ item.name }}</view>
+					<text :class="'cuIcon-' + item.cuIcon"></text>
+				</view>
+			</view>
+			<view class="cu-tabbar-height"></view>
+		</scroll-view>
 	</view>
 </template>
 
 <script>
-	import basic from '@/api/basic';
-	export default {
-		data() {
-			return {
-				elements: [
-					/* {
+import basic from '@/api/basic';
+export default {
+	data() {
+		return {
+			elements: [
+				/* {
 						title: '其他入库',
 						name: 'ICReceive',
 						color: 'cyan',
@@ -81,33 +96,84 @@
 						name: 'dishDeficient',
 						color: 'cyan',
 					},*/
-				],
-			};
-		},
-		onLoad: function (option) {
-			let me = this
-			if(JSON.stringify(option) != "{}"){
-				basic.getSysMenuById(option.id).then(rest => {
-				let data = rest.data
-				this.elements = data
-			})
-		  }
-		},
-		methods: {
-		
+			]
+		};
+	},
+	onLoad: function(option) {
+		let me = this;
+		if (JSON.stringify(option) != '{}') {
+			basic.getSysMenuById(option.id).then(rest => {
+				let data = rest.data;
+				this.elements = data;
+			});
+		}
+	},
+	methods: { 
+		fabClick() {
+			var that = this;
+			let resultA = []; 
+			uni.scanCode({
+				success: function(res) {
+					basic.barcodeScan({ uuid: res.result }).then(reso => {
+							console.log(reso)
+							if (reso.data.chartNumber != null && typeof reso.data.chartNumber != 'undefined') {
+								// 预览图片
+								uni.navigateTo({
+									url: 'warehouse/documentEnquiry'
+								});
+								/* uni.previewImage({
+								  urls: ['https://tz.xinyuanhengye.cn:8088/tz.html?file=' + reso.data.chartNumber + '.pdf'],
+								  longPressActions: {
+								    itemList: ['发送给朋友', '保存图片', '收藏'],
+								    success: function(data) {
+								      console.log('选中了第' + (data.tapIndex + 1) + '个按钮,第' + (data.index + 1) + '张图片');
+								    },
+								    fail: function(err) {
+								      console.log(err.errMsg);
+								    }
+								  }
+								}) */
+								/* uni.downloadFile({
+									url: 'https://tz.xinyuanhengye.cn:8088/tz.html?file=' + reso.data.chartNumber + '.pdf',
+									success: function(resr) {
+										var filePath = resr.tempFilePath;
+										uni.openDocument({
+											filePath: filePath,
+											success: function(rest) {
+												console.log('打开文档成功');
+											}
+										});
+									}
+								}); */
+							} else {
+								uni.showToast({
+									icon: 'none',
+									title: '该二维码没有图号'
+								});
+							}
+						})
+						.catch(err => {
+							uni.showToast({
+								icon: 'none',
+								title: err.msg
+							});
+						});
+				}
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	.page {
-		height: 100vh;
-	}
-	.nav-list{
-		margin-top: 5%;
-	}
-	.nav-title::first-letter {
-	    font-size: 16px;
-	    margin-right: 2px;
-	}
+.page {
+	height: 100vh;
+}
+.nav-list {
+	margin-top: 5%;
+}
+.nav-title::first-letter {
+	font-size: 16px;
+	margin-right: 2px;
+}
 </style>
---
+++ pages/component/warehouse/documentEnquiry.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-gradual-blue" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">文档查询</block>
+		</cu-custom>
+		<web-view style="margin-top: 14%;" :src="allUrl" @message="getMessage"></web-view>
+	</view>
+</template>
+
+<script>
+import basic from '@/api/basic';
+export default {
+	data() {
+		return {
+			src: '',
+			allUrl: '',
+			viewerUrl: 'http://xyy.gzfzdev.com:8866/xyy/web/viewer.html' // 格式化文件流的一个js 文件 文末会分享出来
+		};
+	},
+	onLoad(options) {
+		let fileUrl = encodeURIComponent('https://tz.xinyuanhengye.cn:8088/tz.html?file=2480.pdf'); // encodeURIComponent 函数可把字符串作为 URI 组件进行编码。
+		this.allUrl = this.viewerUrl + '?file=' + fileUrl;
+		console.log(this.allUrl);
+	},
+	methods: {
+		getMessage(e) {
+			let data = e.detail.data;
+			console.log(JSON.stringify(data));
+		}
+	}
+};
+</script>
+
+<style>
+.page {
+	height: 100vh;
+}
+.nav-list {
+	margin-top: 5%;
+}
+.nav-title::first-letter {
+	font-size: 16px;
+	margin-right: 2px;
+}
+</style>
